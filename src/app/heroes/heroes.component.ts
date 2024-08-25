33390a785cfd92d68b6b71d7f2747b4bc5a5692b
Danilo Silva <danilo.silva@msn.com>
2024-08-25 17:48:06 -0300

    chore: Update app component and heroes component templates

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Heroi } from '../heroi';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-
+  heroi: Heroi = {
+    id: 1,
+    nome: 'Homem-Aranha'
+  };
 }
